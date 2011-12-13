351ee49f284f2f8026d7281388caa2fb21a1aa12
Gordon Franke <info@nevalon.de>
2011-12-13 16:16:39 +0100

    fix cs; add/fix phpdocs; reduce code

--- Provider/GoogleProvider.php
+++ Provider/GoogleProvider.php
@@ -2,20 +2,23 @@
 
 namespace Etcpasswd\OAuthBundle\Provider;
 
+use Buzz\Message\Request;
+use Buzz\Message\Response;
 use Etcpasswd\OAuthBundle\Provider\Token\GoogleToken;
 
-use Buzz\Message\Request,
-    Buzz\Message\Response;
 /**
+ * OAuth provider for google
  *
- * @author   Marcel Beerta <marcel@etcpasswd.de>
+ * @author Marcel Beerta <marcel@etcpasswd.de>
+ * @link   http://code.google.com/apis/accounts/docs/OAuth2.html
  */
 class GoogleProvider extends Provider
 {
-
+    /**
+     * {@inheritDoc}
+     */
     public function createTokenResponse($clientId, $secret, $code, $redirectUrl = "")
     {
-
         $url = 'https://www.google.com/accounts/o8/oauth2/token';
 
         $request = new Request(Request::METHOD_POST, $url);
@@ -34,20 +37,19 @@ class GoogleProvider extends Provider
         if (isset($data->error)) {
             return;
         }
-        $expiresAt = time()+$data->expires_in;
+        $expiresAt = time() + $data->expires_in;
 
         $people = 'https://www.googleapis.com/plus/v1/people/me'
             .'?key='.$clientId
             .'&access_token='.$data->access_token;
-        $request = new Request(Request::METHOD_GET, $people);
-        $response = new Response();
-
-        $this->client->send($request, $response);
-        $me = json_decode($response->getContent());
+        $me = json_decode($this->request($people));
 
         return new GoogleToken($me, $data->access_token, $expiresAt);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public function getAuthorizationUrl($clientId, $scope, $redirectUrl)
     {
         return 'https://accounts.google.com/o/oauth2/auth'
@@ -56,5 +58,4 @@ class GoogleProvider extends Provider
             .'&scope='.urlencode($scope)
             .'&response_type=code';
     }
-
 }
